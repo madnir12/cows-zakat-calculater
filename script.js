441b4aa5a1b9f9c7bf651310ee36a6b9f9786218
madnir12 <108262050+madnir12@users.noreply.github.com>
2022-08-04 10:03:21 +0500

    after removing some useless code and adding some functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// adding function to reverse submit behaviour
+document.getElementById("form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+
+}); 
+
+
+// function for cows zakat calculater
 function cowsCounter(){
     let cows = document.getElementById("cows").value;
     let tabi = 0;
@@ -23,41 +31,36 @@ if(cows < 30){
         tadad = cows/30;
         tabi = tadad;
 
-    } else if(cows%40 ==30){
-        let temptd = cows-30;
-        tadad = temptd/40;
-        musin = tadad;
-        tabi++; 
-    }
-    else if(cows%40 ==10){
+    }else if(cows%40 ==10){
         let md = cows%40;
         let amt = cows -md;
         let temptadad = amt/40;
          let tempmusin =temptadad -2;
         musin = tempmusin;
         tabi = 3;
-    }else if(cows%40 < 10){
+    }else if(cows%40 ==20){
+        temptadad = cows-20;
+        tadad = temptadad/40;
+        musin = tadad -1;
+        tabi = +2;
+        
+    } else if(cows%40 ==30){
+        let temptd = cows-30;
+        tadad = temptd/40;
+        musin = tadad;
+        tabi++; 
+    }
+    else if(cows%40 < 10){
         md = cows%40;
         amt = cows-md;
         tadad = amt/40;
         musin = tadad;
-    }else if(cows%30 ==10){
+    }else if(cows%40 >20 && cows%40 < 30){
         md = cows%40;
         amt = cows -md;
         temptadad = amt/40;
-        tempmusin = temptadad -1;
-        musin = tempmusin;
-        tabi = 2;
-    }else if(cows%30 < 20){
-        md = cows%30;
-        amt = cows -md;
-        temptadad = amt/30;
-        tabi = temptadad;
-    }else if(cows%40 < 30){
-        md = cows%40;
-        amt = cows -md;
-        temptadad = amt/40;
-        musin = temptadad;  
+        musin = temptadad -1;
+        tabi = +2;  
     }else if(cows%40 > 30 && cows%40 <40){
       md = cows%40;
       amt = cows -md;
